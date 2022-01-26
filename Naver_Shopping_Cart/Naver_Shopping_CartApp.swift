c61b12516b36695f189f9da77c3286d84d31a7a1
Hyun-je <bigae2@gmail.com>
2022-01-26 16:22:57 +0900

    최초 커밋

---
+++ Naver_Shopping_Cart/Naver_Shopping_CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naver_Shopping_CartApp.swift
+//  Naver_Shopping_Cart
+//
+//  Created by Jaehyeon Park on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Naver_Shopping_CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
